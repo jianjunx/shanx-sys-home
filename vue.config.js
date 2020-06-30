bda89bd415d6bbf26fce31fd5b7f57dfde1a8780
jianjun.xie <jaykenxie@outlook.com>
2020-06-30 18:22:10 +0800

    初始化应用

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const packageName = require("./package.json").name;
+
+module.exports = {
+  devServer: {
+    port: 8081,
+    disableHostCheck: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+    output: {
+      library: `${packageName}-[name]`,
+      libraryTarget: "umd",
+      jsonpFunction: `webpackJsonp_${packageName}`,
+    },
+  },
+};
